499c75ac0e880ebc8039e1ba4462484a41919f7f
KarenAGonzalezL <karengonzalez01@aragon.unam.mx>
2025-05-29 16:40:26 -0600

    Player Animations

--- ProyectoEscapeV3/Assets/Script/JugadorMov.cs
+++ ProyectoEscapeV3/Assets/Script/JugadorMov.cs
@@ -17,20 +17,6 @@ public class JugadorMov : MonoBehaviour
 
     private Animator anima;
 
-    //-Funciones No necesarias para el juego final
-
-    /*private bool saltar;
-    private bool puedeSal = true;
-    public float fuerzaSalto;
-
-    public static float multiSaltoFu = 1;
-    private int multiSalt = 2;
-
-    private float tiempoCoyote = 0.2f;
-    private bool coyoteTime;
-    private float temporizadorCoyote;
-    private bool yaSalto = false;*/
-
     void Start()
     {
         //multiSaltoFu = 1;
@@ -46,91 +32,15 @@ public class JugadorMov : MonoBehaviour
 
         direccionMovimiento = orientacion.forward * movZ + orientacion.right * movX;
 
-        
-        if (movX != 0 || movZ != 0)
-        {
-            anima.SetInteger("velocidad", 1);
-        }
-        else
-        {
-            anima.SetInteger("velocidad", 0);
-        }
-        
+        anima.SetFloat("X", movX);
+        anima.SetFloat("Y", movZ);
 
-        //--Cosas No necesarias para el juego final
-
-        /*if (!puedeSal && coyoteTime && !yaSalto)
-        {
-            temporizadorCoyote += Time.deltaTime;
-            if (temporizadorCoyote > tiempoCoyote)
-            {
-                Debug.Log("Se termino Coyote");
-                coyoteTime = false;
-                multiSalt = 0;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && multiSalt > 0 && PausaJuego.juegoPausa==false)
-        {
-            saltar = true;
-
-            
-        }*/
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(direccionMovimiento.x * velMov * multiplicadorVel * Time.fixedDeltaTime, rb.velocity.y, direccionMovimiento.z * velMov * multiplicadorVel * Time.fixedDeltaTime);
-
-
-        // -- Cosas no necesarias para el juego final
-       /* if (saltar && multiSalt > 0 && coyoteTime)
-        {
-            //anima.SetBool("salte", true);
-            anima.SetTrigger("saltar");
-            yaSalto = true;
-            rb.AddForce(Vector3.up * fuerzaSalto * multiSaltoFu * Time.fixedDeltaTime, ForceMode.Impulse);
-            multiSalt--;
-            saltar = false;
-
-        }*/
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.CompareTag("Suelo"))
-        {
-            // -- Cosas no necesarias para el juego final
-            /* yaSalto = false;
-             puedeSal = true;
-             coyoteTime = true;
-             temporizadorCoyote = 0;
-             multiSalt = 2;*/
-            //anima.SetBool("suelo", true);
-            //Debug.Log("Puede saltar? - " + puedeSal);
-
-        }
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.transform.CompareTag("Suelo"))
-        {
-            // -- Cosas no necesarias para el juego final
-            //puedeSal = true;
-            //.SetBool("suelo", true);
-
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.transform.CompareTag("Suelo"))
-        {
-            
-            //puedeSal = false;
-            anima.SetBool("suelo", false);
-        }
-    }
 
 }
